59a10dbf13861363752c445389e6c8d2b55109c8
Jstali <stalinnithin3434@gmail.com>
2026-04-01 15:32:22 +0530

    Update: API configuration changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ const API_HOST = 'https://fadev-im-fileconnect-frontend-uks03.azurewebsites.net'
 const API_CODE = 'REDACTED_DTC_API_CODE_V2=';
 const SAP_API_CODE = 'REDACTED_SAP_API_CODE_V2=';
 
-const DTC_AUDIT_API = `${API_HOST}/api/dtcAuditAPI?code=${API_CODE}`;
+const DTC_AUDIT_API = `${API_HOST}/api/dtcAuditApi?code=${API_CODE}`;
 const SAP_AUDIT_API = `${API_HOST}/api/sapAuditApi?code=${SAP_API_CODE}`;
 
 const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:4000';
@@ -25,7 +25,7 @@ const handleResponse = async (res) => {
 
 export const fetchDtcSubscriptions = async () => {
   try {
-    const apiUrl = `${API_HOST}/api/dtcSubscriptionAPI?code=${API_CODE}`;
+    const apiUrl = `${API_HOST}/api/dtcSubscriptionApi?code=${API_CODE}`;
 
     const res = await fetch(apiUrl, { method: 'GET' });
 
